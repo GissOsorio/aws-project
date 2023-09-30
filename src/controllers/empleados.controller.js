3a3f34829780f513e92d01c62012b5eeb96440ea
Gisela Osorio <giss_ot@hotmail.com>
2023-09-30 11:22:11 -0500

    Mensaje

--- src/controllers/empleados.controller.js
+++ src/controllers/empleados.controller.js
@@ -5,7 +5,7 @@ empleadoCtrl.getEmpleados = async (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "content-type");
     res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
-    const empleados = "Welcome Taller";
+    const empleados = "Hola ❤️!";
     res.json(empleados);
 };
 
