88527d1f905dd0e1b6db47b0b93011039dd71c34
Gisela Osorio <giss_ot@hotmail.com>
2023-09-30 10:56:37 -0500

    cors

--- src/controllers/empleados.controller.js
+++ src/controllers/empleados.controller.js
@@ -2,6 +2,9 @@ const empleadoCtrl={};
 
 
 empleadoCtrl.getEmpleados = async (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
     const empleados = "Welcome Taller";
     res.json(empleados);
 };
